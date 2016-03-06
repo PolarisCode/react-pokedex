70b87c9fb49817fbdc1ad207fb061ddba59dd95e
Imran Aliyev <imran.aliyev@gmail.com>
2016-03-06 17:09:26 +0400

    Entry point for jsx components was created and configured.

---
+++ src/components/SearchBox.jsx
@@ -0,0 +1,11 @@
+var React = require('react');
+
+var SearchBox = React.createClass({
+  render: function(){
+    return (
+      <div> search functionality goes here </div>
+    );
+  }
+});
+
+module.exports = SearchBox;
---
+++ src/main.jsx
@@ -0,0 +1,6 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+var SearchBox = require('./components/SearchBox.jsx');
+
+
+ReactDOM.render(<SearchBox/>, document.getElementById('top-container'));
